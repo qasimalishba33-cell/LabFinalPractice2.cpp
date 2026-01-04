ff689fd6cc0358d3021695a5d0297a3ca0e49724
Alishba Qasim <qasimalishba33@gmail.com>
2026-01-04 11:40:38 +0500

    Basic Pointers concept added

---
+++ p1.cpp
@@ -0,0 +1,13 @@
+//Pointer Practice
+#include<iostream>
+using namespace std;
+int main(){
+ 
+ 
+ 	int a =10;
+  int *p = &a;
+  cout<<*p<<endl;
+  cout<<p<<endl; //Address of variable 
+ 
+  cout<<p+1; //Adress change 
+  }
